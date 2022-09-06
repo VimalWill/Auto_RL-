cd7c447e62f8ca73de2057214182b66a9b298f98
VimalWill <vimalwilliam99@gmail.com>
2022-09-06 23:55:17 +0530

    added camera

--- RLenv.py
+++ RLenv.py
@@ -37,15 +37,45 @@ class env():
         self.model_3 = self.blueprint_library.filter("model3")[0]
     
     def reset(self):
-        self.imu_hist = []
+        self.data_dict = dict()
         self.actor_list = []
 
         self.transform = random.choice(self.world.get_map().get_spawn_points())
         self.vehicle = self.world.spawn_actor(self.model_3,self.transform)
         self.actor_list.append(self.vehicle)
 
+        transform_c = carla.Transform(carla.Location(x=2,z=0.7))
+        self.rgb_cam = self.blueprint_library.find("sensor.camera.rgb")
+        self.rgb.set_attribute("image_size_x",f"{self.im_width}")
+        self.rbg.set_attribute("image_size_y",f"{self.im_height}")
+        self.rgb.set_attribute("fov",f"110")
+        self.cam_Sensor = self.world.spawn_actor(self.rgb_cam,transform_c,attach_to=self.vehicle)
+        self.actor_list.append(self.cam_Sensor)
+        self.cam_Sensor.listen(lambda data_c: self.camera_callback(data_c))
+
         transform_s = carla.Transform(carla.Location(z=2))
         imu_sensor = self.blueprint_library.find("sensor.other.imu")
         self.imu_sensor = self.world.spawn_actor(imu_sensor,transform_s,attach_to = self.vehicle)
-        
+        self.actor_list.append(self.imu_sensor)
+        self.imu_sensor.listen(lambda data: self.imu_callback(data))
+    
+    
+    def imu_callback(self,data):
+        self.data_dict['imu'] = {
+            'gyro' : data.gyroscope,
+            'accel' : data.accelerometer,
+            'compass': data.compass
+
+        }
+
+    def camera_callback(self,data_c):
+        i = np.array(data_c.raw_data)
+        i2 = i.reshape((self.im_height,self.im_width,4))
+        i3 = i2[:,:,:3]
+        if self.SHOW_CAM:
+            cv2.imshow("",i3)
+            cv2.waitKey(1)
+        self.front_camera = i3 
+
+            
 
