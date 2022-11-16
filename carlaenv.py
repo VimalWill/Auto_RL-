41c194802db8b6076ad091a9af4c5a05e80f26e3
VimalWill <vimalwilliam99@gmail.com>
2022-11-16 22:09:57 +0530

    added the tensorboard

--- carlaenv.py
+++ carlaenv.py
@@ -13,6 +13,7 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import Model
 from keras.layers import Dense, Activation, Input
+from keras.callbacks import TensorBoard
 from keras.applications import ResNet50
 
 from rl.agents import DQNAgent
@@ -151,10 +152,45 @@ class carlaEnv(Env):
     def collision_callback(self,event):
         self.collision_hist.append(event)
 
+class ModifiedTensorBoard(TensorBoard):
+    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    def __init__(self, name, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.create_file_writer(self.log_dir)
+        self._log_write_dir = os.path.join(self.log_dir, name)
+
+    # Overriding this method to stop creating default log writer
+    def set_model(self, model):
+        pass
+
+    # Overrided, saves logs with our step number
+    # (otherwise every .fit() will start writing from 0th step)
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    # Overrided
+    # We train for one batch only, no need to save anything at epoch end
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    # Overrided, so won't close writer
+    def on_train_end(self, _):
+        pass
+
+    def on_train_batch_end(self, batch, logs=None):
+        pass
+
+    # Custom method for saving own metrics
+    # Creates writer, writes custom metrics and closes writer
+    def update_stats(self, **stats):
+        self._write_logs(stats, self.step)
+
 class DqlModel():
     def __init__(self):
         self.state = carlaEnv().observation_space.shape 
         self.action = carlaEnv().action_space.n 
+        self.tensorboard = ModifiedTensorBoard(log_dir="./logs")
 
     def cnn_model(self):
 
