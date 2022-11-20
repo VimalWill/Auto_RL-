060272c0cdba469ef75d1c0d00e49c692ecc5abf
VimalWill <vimalwilliam99@gmail.com>
2022-11-20 12:39:21 +0530

    updated the env

--- carlaenv.py
+++ carlaenv.py
@@ -1,24 +1,14 @@
-import carla 
+import carla
 from gym import Env
 from gym.spaces import Box, Discrete
-import numpy as np 
-import glob 
-import sys 
-import time 
-import random 
-import cv2 
+import numpy as np
+import glob
+import sys
+import time
+import random
+import cv2
 import os
 
-import tensorflow as tf 
-from tensorflow import keras
-from keras import Model
-from tensorboard import ModifiedTensorBoard
-from keras.layers import Dense, Activation, Input
-from keras.applications import ResNet50
-
-from rl.agents import DQNAgent
-from rl.policy import BoltzmannGumbelQPolicy
-from rl.memory import SequentialMemory
 
 try:
     sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
@@ -29,12 +19,13 @@ except IndexError:
     pass
 
 SHOW_PREVIEW = False
-ACTIONS  = 3
-IM_WIDTH =  640
+ACTIONS = 3
+IM_WIDTH = 640
 IM_HEIGHT = 480
-SECONDS_PER_EPISODE = 60 
+SECONDS_PER_EPISODE = 60
 
-class carlaEnv(Env):
+
+class Carlaeni(Env):
     metadata = {'render.modes': ['human']}
 
     SHOW_CAM = SHOW_PREVIEW
@@ -46,81 +37,89 @@ class carlaEnv(Env):
     def __init__(self):
         super().__init__()
 
-        #number of actions (Straigth, Left and Right)
+        # number of actions (Straigth, Left and Right)
         self.action_space = Discrete(ACTIONS)
-        self.image_shape = (self.im_height,self.im_width,3)
-        self.observation_space = Box(low = 0, high=255,shape=self.image_shape)
+        self.image_shape = (self.im_height, self.im_width, 3)
+        self.observation_space = Box(low=0, high=255, shape=self.image_shape)
 
-        #init the carla 
-        self.client = carla.Client('localhost',2000)
+        # init the carla
+        self.client = carla.Client('localhost', 2000)
         self.client.set_timeout(10.0)
         self.world = self.client.get_world()
         self.blueprint_library = self.world.get_blueprint_library()
         self.model_3 = self.blueprint_library.filter("model3")[0]
 
-    def step(self,action):
-        
+    def step(self, action):
+
         if action == 0:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0,steer = -1*self.STEER_AMT))
+            self.vehicle.apply_control(carla.VehicleControl(
+                throttle=1.0, steer=-1*self.STEER_AMT))
         elif action == 1:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0,steer = 0))
+            self.vehicle.apply_control(
+                carla.VehicleControl(throttle=1.0, steer=0))
         elif action == 2:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0,steer= 1*self.STEER_AMT))
-        
-        #calculating the acceleration 
-        accel = self.data_dict['imu']['accel'] - carla.Vector3D(x=0,y=0,z=9.81)
+            self.vehicle.apply_control(carla.VehicleControl(
+                throttle=1.0, steer=1*self.STEER_AMT))
+
+        # calculating the acceleration
+        accel = self.data_dict['imu']['accel'] - \
+            carla.Vector3D(x=0, y=0, z=9.81)
         accel_mag = accel.length()
-        
-        #computing reward 
+
+        # computing reward
         if len(self.collision_hist) == 0 and (accel_mag < 50 and accel_mag > 35):
-            Done = False 
+            Done = False
             Reward = 5
 
         elif self.episode_start + SECONDS_PER_EPISODE < time.time():
-            Done = True 
+            Done = True
             Reward = 2
 
         else:
-            Done = False 
+            Done = False
             Reward = -10
 
         info = {}
 
-        return self.front_camera,Reward, Done, info #observation, reward, status, extra information 
-        
-
-    def render(self, mode='human'):
-        return #nothing
+        # observation, reward, status, extra information
+        return self.front_camera, Reward, Done, info
 
     def reset(self):
         self.data_dict = dict()
         self.actor_list = []
-        self.collision_hist  = []
+        self.collision_hist = []
 
         self.transform = random.choice(self.world.get_map().get_spawn_points())
-        self.vehicle = self.world.spawn_actor(self.model_3,self.transform)
+        self.vehicle = self.world.spawn_actor(self.model_3, self.transform)
         self.actor_list.append(self.vehicle)
 
-        transform_c = carla.Transform(carla.Location(x=2,z=0.7))
-        self.rgb_cam = self.blueprint_library.find("sensor.camera.rgb")
-        self.rgb.set_attribute("image_size_x",f"{self.im_width}")
-        self.rbg.set_attribute("image_size_y",f"{self.im_height}")
-        self.rgb.set_attribute("fov",f"110")
-        self.cam_Sensor = self.world.spawn_actor(self.rgb_cam,transform_c,attach_to=self.vehicle)
-        self.actor_list.append(self.cam_Sensor)
-        self.cam_Sensor.listen(lambda data_c: self.camera_callback(data_c))
+        self.rgb_cam = self.blueprint_library.find('sensor.camera.rgb')
+        self.rgb_cam.set_attribute("image_size_x", f"{self.im_width}")
+        self.rgb_cam.set_attribute("image_size_y", f"{self.im_height}")
+        self.rgb_cam.set_attribute("fov", f"110")
 
-        self.vehicle.apply_control(carla.Vehiclecontrol(throttle=0.0,brake = 0.0))
+        transform = carla.Transform(carla.Location(x=2.5, z=0.7))
+        self.cam_sensor = self.world.spawn_actor(
+        self.rgb_cam, transform, attach_to=self.vehicle)
+        self.actor_list.append(self.cam_sensor)
+        self.cam_sensor.listen(lambda data_c: self.camera_callback(data_c))
+
+        self.vehicle.apply_control(
+            carla.VehicleControl(throttle=0.0, brake=0.0))
         time.sleep(4.0)
 
-        collision_sensor = self.blueprint_library.find("sensor.other.collision")
-        self.collision_sensor = self.world.spawn_actor(collision_sensor,transform_c,to_attach = self.vehicle)
+        collision_sensor = self.blueprint_library.find(
+            "sensor.other.collision")
+        self.collision_sensor = self.world.spawn_actor(
+            collision_sensor, transform, attach_to=self.vehicle)
         self.actor_list.append(self.collision_sensor)
-        self.collision_sensor.listen(lambda event: self.collision_callback(event))
+        self.collision_sensor.listen(
+            lambda event: self.collision_callback(event))
 
         transform_s = carla.Transform(carla.Location(z=2))
         imu_sensor = self.blueprint_library.find("sensor.other.imu")
-        self.imu_sensor = self.world.spawn_actor(imu_sensor,transform_s,attach_to = self.vehicle)
+        self.imu_sensor = self.world.spawn_actor(
+            imu_sensor, transform_s, attach_to=self.vehicle)
         self.actor_list.append(self.imu_sensor)
         self.imu_sensor.listen(lambda data: self.imu_callback(data))
 
@@ -128,58 +127,32 @@ class carlaEnv(Env):
             time.sleep(0.01)
 
         self.episode_start = time.time()
-        self.vehicle.apply_control(carla.Vehiclecontrol(throttle=0.0,brake=0.0))
+        self.vehicle.apply_control(
+            carla.VehicleControl(throttle=0.0, brake=0.0))
 
-        return self.front_camera #observation 
+        return self.front_camera  # observation
 
-    def imu_callback(self,data):
+    def imu_callback(self, data):
         self.data_dict['imu'] = {
-            'gyro' : data.gyroscope,
-            'accel' : data.accelerometer,
+            'gyro': data.gyroscope,
+            'accel': data.accelerometer,
             'compass': data.compass
 
         }
-    
-    def camera_callback(self,data_c):
-        i = np.array(data_c.raw_data)
-        i2 = i.reshape((self.im_height,self.im_width,4))
-        i3 = i2[:,:,:3]
+
+    def camera_callback(self, image):
+        i = np.array(image.raw_data)
+        #print(i.shape)
+        i2 = i.reshape((self.im_height, self.im_width, 4))
+        i3 = i2[:, :, :3]
         if self.SHOW_CAM:
-            cv2.imshow("",i3)
+            cv2.imshow("", i3)
             cv2.waitKey(1)
-        self.front_camera = i3 
-    
-    def collision_callback(self,event):
+        self.front_camera = i3
+
+    def collision_callback(self, event):
         self.collision_hist.append(event)
 
+    def render(self,mode="human",close=False):
+        return #nothing 
 
-class DqlModel():
-    def __init__(self):
-        self.state = carlaEnv().observation_space.shape 
-        self.action = carlaEnv().action_space.n 
-        self.tensorboard = ModifiedTensorBoard(log_dir="./logs")
-
-    def cnn_model(self):
-
-        #using Resnet pretrained model 
-        self.base_model = ResNet50(input_tensor=self.state,weights="imagenet",include_top=False)
-        self.x = self.base_model.output
-        self.x = Dense(64, Activation = "relu")(self.x)
-        self.x = Dense(32, Activation = "relu")(self.x)
-        self.x = Dense(64, Activation = "relu")(self.x)
-
-        self.output_layer = Dense(self.action,Activation='linear')(self.x)
-        self.model = Model(input = self.base_model.input, output = self.output_layer)
-
-    def DqnModel(self):
-        self.policy = BoltzmannGumbelQPolicy()
-        self.memory = SequentialMemory(limit = 50000,window_length = 1)
-        
-        dqn = DQNAgent(model = self.model, memory = self.memory,policy=self.policy, 
-        nb_actions = self.action,nb_steps_warmup=10,target_model_update = 1e-2)
-
-        return dqn
-        
-     
-
-    
--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-from carlaenv import carlaEnv, DqlModel
+from CarlaEnv import Carlaeni
 from keras.optimizers import Adam
+import tensorflow as tf
+from tensorflow import keras
+from keras import Model
+from tensorboard import ModifiedTensorBoard
+from keras.layers import Dense, Activation, Input, GlobalAveragePooling2D
+from keras.applications import ResNet50
+import cv2 
 
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannGumbelQPolicy
+from rl.memory import SequentialMemory
+
+
+
+state = Carlaeni().observation_space.shape
+action = Carlaeni().action_space.n
+        
+def cnn_model(state,action):
+    base_model = ResNet50(
+    input_shape=state, weights="imagenet", include_top=False)
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(64, activation="relu")(x)
+    x = Dense(32, activation="relu")(x)
+    x = Dense(64, activation="relu")(x)
+    output_layer = Dense(action, activation='softmax')(x)
+    model = Model(inputs=base_model.input,outputs=output_layer)
+
+    return model 
+
+model = cnn_model(state,action)
+    
 def main():
     
     """
@@ -13,13 +44,11 @@ def main():
 
     #init the carla env 
     env = carlaEnv()
+    #ensuring the working 
+    current_state = env.reset()
+    cv2.imshow(f'Agent - preview', current_state)
+    cv2.waitKey(2)
 
-    #calling the DQL model 
-    dql = DqlModel().DqnModel()
-
-    dql.compile(Adam(lr=1e-3),metrics=['mae'])
-    dql.fit(env,nb_steps=50000,visualize = True, verbose=1)
-    
 
 if __name__ == "__main__":
     main()
